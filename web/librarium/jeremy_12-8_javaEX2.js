06aa9ad975e71fa3cc6d55569f26e25bf1a17239
JeremyF5737 <jjf5737@psu.edu>
2020-12-09 16:54:19 -0500

    attempting to fix the html issue, by making a copy of the svg_output folder in the librarium folder

---
+++ web/librarium/jeremy_12-8_javaEX2.js
@@ -0,0 +1,39 @@
+function changeBackground() {
+    
+    var text = document.getElementById("items").stylebackgroundColor = "red";
+}
+
+function changeBackground() {
+    
+    var text = document.getElementById("group").stylebackgroundColor = "blue";
+    
+    function changeBackground() {
+   }
+    var text = document.getElementById("loc").stylebackgroundColor = "yellow";
+   }
+    function changeBackground() {
+    
+    var text = document.getElementById("persons").stylebackgroundColor = "green";
+}
+
+/*JJF: These changebackground functions are going to go into the linked list elements
+and use their id value, so when the mouse hovers over them, the background behind the 
+text will change colors.*/
+
+
+
+
+
+
+
+
+
+
+function backToNormal() {
+    
+    var text = document.getElementById("loc").stylebackgroundColor = "";
+    
+}
+
+/*JJF: When the mouse moves off of the changebackground functions, the background
+will revert to normal, which is why the stylebackgroundcolor has no value*/
