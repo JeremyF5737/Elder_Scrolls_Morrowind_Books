ec5efd126c52fb1ff4de46a00fa4ff21fbd30fe7
Tricky-Keys <pkiceking@gmail.com>
2020-12-08 21:35:49 -0500

    updated css

---
+++ web/librarium/showHideTables.js
@@ -0,0 +1,35 @@
+function toggleitems() {
+  var x = document.getElementById("items");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else { 
+    x.style.display = "none";
+  }
+}
+
+function toggleloc() {
+  var x = document.getElementById("loc");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else { 
+    x.style.display = "none";
+  }
+}
+
+function togglegroup() {
+  var x = document.getElementById("group");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else { 
+    x.style.display = "none";
+  }
+}
+
+function togglepersons() {
+  var x = document.getElementById("persons");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else { 
+    x.style.display = "none";
+  }
+}
